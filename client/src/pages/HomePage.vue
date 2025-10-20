23885d2aa152d4b7bffe592f23f3c6b713d42971
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-20 11:11:36 -0600

    hero section, getting cryptids

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,75 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+import { cryptidsService } from '@/services/CryptidsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(getCryptids)
+
+async function getCryptids() {
+  try {
+    await cryptidsService.getCryptids()
+  } catch (error) {
+    Pop.error(error)
+    logger.error('COULD NOT GET CRYPTIDS', error)
+  }
+}
 
 </script>
 
 <template>
-  <Example />
+  <section class="container-fluid spooky-woods-bg">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="mx-5 mt-5">
+          <h2>Terrestrials</h2>
+          <p>
+            A terrestrial cryptid is a creature that exists on land but has not been “scientifically” proven. These
+            creatures often stem from folklore, mythology, or anecdotal evidence. Unlike aquatic cryptids, like the Loch
+            Ness Monster, terrestrial cryptids inhabit forests, mountains, or other land-based environments.
+          </p>
+          <p>
+            Examples of Terrestrials:
+            <br>
+            Bigfoot, El Chupacabra and Mothman
+          </p>
+        </div>
+      </div>
+      <div class="col-md-4 pe-0 text-end">
+        <img src="@/assets/img/little_freak.png" alt="A very mysterious little freak"
+          class="img-fluid terrestrial-cryptid">
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row">
+      <div class="col-12 my-5">
+        <h1 class="italiana-font display-1">CRYPTIDS</h1>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
+.terrestrial-cryptid {
+  transform: scaleX(-1);
+  filter: hue-rotate(185deg);
+  opacity: .8;
+}
 
+.spooky-woods-bg {
+  background-image: url(@/assets/img/spooky_woods.jpg);
+  background-size: cover;
+  background-position: center;
+
+  .row {
+    backdrop-filter: sepia(.6);
+    background-color: rgba(43, 43, 43, 0.429);
+  }
+
+  .col-md-8 {
+    text-shadow: 0 1px black;
+    font-weight: bold;
+  }
+}
 </style>
---
+++ client/src/services/CryptidsService.js
@@ -0,0 +1,11 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class CryptidsService {
+  async getCryptids() {
+    const response = await api.get('api/cryptids')
+    logger.log('GOT CRYPTIDS 💀🦇🦟🕷️', response.data)
+  }
+}
+
+export const cryptidsService = new CryptidsService()
